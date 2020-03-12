4864e1026a20b420212c6957bc0401b43d435fb3
A.G.Sudani <36556422+ahmedsmrt@users.noreply.github.com>
2020-03-12 01:24:13 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,7 +15,14 @@ var cursor2 = document.getElementById('cursor2'),
 wrap_1 = document.getElementById('wrap-1'),
 logo = document.getElementById('logo'),
 cursor3 = document.getElementById('cursor3'),
-cursor4 = document.getElementById('cursor4');
+cursor4 = document.getElementById('cursor4'),
+cursor4 = document.getElementById('cursor4'),
+navWrap = document.getElementById('navWrap'),
+lineWrap = document.getElementById('lineWrap'),
+linksArr = navWrap.querySelectorAll('a'),
+linesArr = lineWrap.querySelectorAll('div'),
+link1 = document.getElementById('link1'),
+line1 = document.getElementById('line1');
 anchors.forEach((e) => {
     e.addEventListener('mouseover', function(event) {
         cursor2.classList.toggle('ancLeft');
@@ -32,6 +39,31 @@ anchors.forEach((e) => {
         cursor4.classList.toggle('ancCenter');
     },false )
 })
+
+
+function arrLooper(e) {
+    for (var i = 0; i< e.length; i++) {
+        e[i].addEventListener('mouseover', function(event) {
+            event = linesArr;
+            linesArr[i].style.transition ='.4s';
+            linesArr[i].style.transform = 'translateX(100%)';
+        })
+        console.log(e[i]);
+    }
+}
+
+arrLooper(linksArr);
+
+// Nav
+
+link1.addEventListener('mouseover', function(event) {
+    line1.style.transition ='.4s';
+    line1.style.transform = 'translateX(100%)'; 
+});
+link1.addEventListener('mouseleave', function(event) {
+    line1.style.transform = 'translateX(-30%)'; 
+});
+
 
 
 
